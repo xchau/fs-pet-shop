53651f83029ed47c2d98a8904d13e3cb92875f8d
Minh Chau <xiu.chau@gmail.com>
2016-12-14 15:34:24 -0800

    Add get response for /pets

--- expressServer.js
+++ expressServer.js
@@ -4,4 +4,31 @@ const fs = require('fs');
 const path = require('path');
 const petsJSONPath = path.join(__dirname, 'pets.json');
 
-const 
+const express = require('express');
+const app = express();
+
+app.disable('x-powered-by');
+
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+
+app.get('/pets', (req, res) => {
+  fs.readFile(petsJSONPath, 'utf8', (err, petsJSON) => {
+    if (err) {
+      console.error(err.stack);
+
+      return res.sendStatus(500);
+    }
+
+    const pets = JSON.parse(petsJSON);
+
+    res.send(pets);
+  });
+});
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+  console.log(`Listening to port ${port}`);
+});
