a0d576f253056467b0ebd6780d84baf87192719a
Minh Chau <xiu.chau@gmail.com>
2016-12-14 16:15:40 -0800

    Allow post requests to add new pets

--- expressServer.js
+++ expressServer.js
@@ -27,6 +27,42 @@ app.get('/pets', (req, res) => {
   });
 });
 
+app.post('/pets', (req, res) => {
+  fs.readFile(petsJSONPath, 'utf8', (readErr, petsJSON) => {
+    if (readErr) {
+      console.error(readErr.stack);
+
+      return res.sendStatus(500);
+    }
+
+    const pets = JSON.parse(petsJSON);
+    const name = req.body.name;
+    const kind = req.body.kind;
+    const age = Number.parseInt(req.body.age);
+
+    if (Number.isNaN(age) || !age || !kind || !name) {
+      return res.sendStatus(400);
+    }
+
+    const pet = { name, kind, age };
+
+    pets.push(pet);
+
+    const newPetsJSON = JSON.stringify(pets);
+
+    fs.writeFile(petsJSONPath, newPetsJSON, (writeErr) => {
+      if (writeErr) {
+        console.error(writeErr.stack);
+
+        return res.sendStatus(500);
+      }
+
+      res.set('Content-Type', 'application/json');
+      res.send(JSON.stringify(pet));
+    });
+  });
+});
+
 app.get('/pets/:id', (req, res) => {
   fs.readFile(petsJSONPath, 'utf8', (err, petsJSON) => {
     if (err) {
@@ -42,9 +78,13 @@ app.get('/pets/:id', (req, res) => {
       return res.sendStatus(404);
     }
 
-    res.set('Content-Type', 'text/plain');
+    res.set('Content-Type', 'application/json');
     res.send(pets[index]);
   });
+});
+
+app.use((req, res) => {
+  res.sendStatus(404);
 });
 
 const port = process.env.PORT || 8000;
@@ -52,3 +92,5 @@ const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`Listening to port ${port}`);
 });
+
+module.exports = app;
